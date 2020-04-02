f73f9339706e2d2e27d51fcd45cc70f23def6097
Rodrigo Viola <rv.rodrigo.viola@gmail.com>
2020-04-02 21:30:51 +0200

    alpha v0.23

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
+function onMouseOut(event) {
+    if (
+        event.clientY < 50 &&
+        event.relatedTarget == null &&
+        event.target.nodeName.toLowerCase() !== 'select') {
+            document.removeEventListener("mouseout", onMouseOut);
+            document.getElementById("popup").style.display = "flex";
+        }
+      }
 
+      document.addEventListener("mouseout", onMouseOut);
+
+      var modal = document.getElementById('popup');
+      var close = document.getElementById("close");
+      var boton = document.getElementById("popup__btn");
+
+      close.onclick = function() {
+          modal.style.display = "none";
+      }
+      boton.onclick = function() {
+          modal.style.display = "none";
+          window.location.href='#contact-form';
+      }
+      window.onclick = function(event) {
+          if (event.target === modal) {
+              modal.style.display = "none";
+          }
+      }
